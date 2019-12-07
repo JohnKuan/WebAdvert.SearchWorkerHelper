d5269a0303cfe51c2007b74b0ecbb443602e4b39
JohnKuan <founders2008@hotmail.com>
2019-12-07 10:55:49 +0800

    filling elastic search helper

---
+++ WebAdvert.SearchWorker/MappingHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AdvertApi.Models.Messages;
+using WedbAdvert.SearchWorker;
+
+namespace WebAdvert.SearchWorker
+{
+    public static class MappingHelper
+    {
+
+        public static AdvertType Map(AdvertConfirmedMessage message)
+        {
+            var doc = new AdvertType
+            {
+                Id = message.Id,
+                Title = message.Title,
+                CreationDateTime = DateTime.UtcNow
+            };
+            return doc;
+        }
+
+        
+    }
+}
--- WebAdvert.SearchWorker/SearchWorker.cs
+++ WebAdvert.SearchWorker/SearchWorker.cs
@@ -1,5 +1,9 @@
-﻿using Amazon.Lambda.Core;
+﻿using AdvertApi.Models.Messages;
+using Amazon.Lambda.Core;
 using Amazon.Lambda.SNSEvents;
+using Nest;
+using Newtonsoft.Json;
+using WedbAdvert.SearchWorker;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
@@ -7,11 +11,25 @@ namespace WebAdvert.SearchWorker
 {
     public class SearchWorker
     {
+        private readonly IElasticClient _client;
+
+        public SearchWorker (IElasticClient client)
+        {
+            _client = client;
+        }
+
+        public SearchWorker() : this(ElasticSearchHelper.GetInstance(ConfigurationHelper.Instance))
+        {
+
+        }
+
         public void Function(SNSEvent snsEvent, ILambdaContext context)
         {
             foreach (var record in snsEvent.Records)
             {
                 context.Logger.LogLine(record.Sns.Message);
+                var message = JsonConvert.DeserializeObject<AdvertConfirmedMessage>(record.Sns.Message);
+                var advertDocument = MappingHelper.Map(message);
             }
 
         }
